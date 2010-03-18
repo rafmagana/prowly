595905741acb18655639f34801e50cf20a7eeb8d
Rafael Magana <raf.magana@gmail.com>
2010-03-18 02:18:06 -0600

    extracted method Response.parse_xml from Reponse.map_xml

--- lib/prowly/response.rb
+++ lib/prowly/response.rb
@@ -2,44 +2,45 @@ require 'rexml/document'
 
 module Prowly
   
+  #This is a Prowl API response wrapper
   class Response
   
     attr_writer :response
     attr_accessor :http_response
 
-    def initialize(xml_response, full_http_response)    
+    def initialize(xml_response, full_http_response)
       @response = xml_response
       @http_response = full_http_response
       Response.map_xml @response
     end
   
     def self.map_xml(response)
-      #parse xml
-      data = REXML::Document.new response
-      response_info = data.root[1]
-    
+      response_info = parse_xml(response)
+      
       #define dynamic methods based on the prowl api response
       #posible methods on success: code, remaining, resetdate
       #posible methods on error: code
       response_info.attributes.each do |key, value|
         add_instance_method(key, value)
       end
-    
+      
+      response_info_name = response_info.name
       #define a method named status and it'll return "success" or "error"
-      Response.add_instance_method(:status, response_info.name)
+      add_instance_method(:status, response_info_name)
     
-      if response_info.name == "success"
-        boolean_status = true
-      elsif response_info.name == "error"
-        boolean_status = false
-      end
+      boolean_status = response_info_name == "success" ? true : false
     
       add_instance_method(:message, response_info.text) unless boolean_status
     
       add_instance_method(:succeeded?, boolean_status)
       true
     end
-  
+    
+    def self.parse_xml(response)
+      data = REXML::Document.new response
+      data.root[1]
+    end
+    
     #define dynamic methods
     def self.add_instance_method(name, content)
       define_method(name) { content }
